ab449bff6adcbaf3047bbfde208924c08a2e0864
kunal1274 <kunal1274@gmail.com>
2024-11-07 19:29:48 +0530

    this is frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl text-red-500 font-bold flex justify-center space-x-4 items-start">
+        <div>
+          <p>Udyamen hi sidhyanti, karyaani naa manorathai |</p>
+          <p> Naa hi suptasya Sinhansya , pravishanti mukhen mrigah ||</p>
+        </div>
+        <div>Om Gan Ganpataye Namo Namah</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
